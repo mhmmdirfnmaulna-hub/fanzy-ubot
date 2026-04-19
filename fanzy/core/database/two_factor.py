0c99fb64bb96843f71865a588f2d8dd5557e7cec
mhmmdirfnmaulna-hub <mhmmdirfnmaulna@gmail.com>
2026-04-19 14:17:24 +0700

    Upload via Userbot - fanzy/core/database/two_factor.py

---
+++ fanzy/core/database/two_factor.py
@@ -0,0 +1,23 @@
+from fanzy.core.database import mongodb
+
+factor = mongodb["fanzy"]["factor"]
+
+
+async def get_two_factor(user_id):
+    user = await factor.users.find_one({"_id": user_id})
+    if user:
+        return user.get("expire_date")
+    else:
+        return None
+
+
+async def set_two_factor(user_id, expire_date):
+    await factor.users.update_one(
+        {"_id": user_id}, {"$set": {"expire_date": expire_date}}, upsert=True
+    )
+
+
+async def rem_two_factor(user_id):
+    await factor.users.update_one(
+        {"_id": user_id}, {"$unset": {"expire_date": ""}}, upsert=True
+    )
